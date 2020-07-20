1955cfdfa67b0b45b61817bb4494c48a80d43cfb
joshni <joshnisaldanha1998@gmail.com>
2020-07-20 15:29:25 +0530

    Upload online menu

---
+++ pro/app/Http/Controllers/homecontroller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\admin;
+
+class homecontroller extends Controller
+{
+    public function adddqry(Request $req)
+    {
+    	$stu=new admin();
+    	$stu->username=$req->input('name');
+    	$stu->password=$req->input('password');
+    
+    if($stu->save())
+    	{
+    		echo "
+    		<script>
+    		alert('inserted');
+    		window.location='home';
+    		</script>
+    		";
+    	}
+    }
+}
